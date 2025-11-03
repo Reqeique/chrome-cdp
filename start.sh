9047d55afbb1ae9e86688ada27ca11cabd51b91b
Reqeique <95624174+Reqeique@users.noreply.github.com>
2025-11-03 09:22:26 +0300

    Add start script to launch Chrome and nginx

---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+# Start Chrome bound to localhost so only the proxy can reach it
+chromium-browser \
+  --headless \
+  --remote-debugging-address=127.0.0.1 \
+  --remote-debugging-port=9222 \
+  --no-sandbox \
+  --disable-dev-shm-usage \
+  --disable-gpu \
+  --disable-software-rasterizer \
+  --remote-allow-origins=* \
+  --allow-insecure-localhost &
+
+# Run nginx in foreground
+exec nginx -g "daemon off;"
