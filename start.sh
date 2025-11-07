076e0b413db2a00ee77b93eb6b6effa9a0f19bcd
Reqeique <95624174+Reqeique@users.noreply.github.com>
2025-11-07 08:47:48 +0300

    Comment out chromium-browser flags in start.sh
    Comment out various chromium-browser flags for testing.

--- start.sh
+++ start.sh
@@ -6,14 +6,14 @@ chromium-browser \
   --headless \
   --remote-debugging-address=127.0.0.1 \
   --remote-debugging-port=9222 \
-  --no-sandbox \
-  --disable-dev-shm-usage \
-  --disable-gpu \
-  --disable-software-rasterizer \
-  --remote-allow-origins=* \
-  --allow-insecure-localhost \
-  --disable-web-security \
-  --disable-features=IsolateOrigins,site-per-process &
+  # --no-sandbox \
+  # --disable-dev-shm-usage \
+  # --disable-gpu \
+  # --disable-software-rasterizer \
+  # --remote-allow-origins=* \
+  # --allow-insecure-localhost \
+  # --disable-web-security \
+  # --disable-features=IsolateOrigins,site-per-process &
 
 # Run nginx in foreground
 exec nginx -g "daemon off;"
