2710d01cfe5cb9be666bb9fb54418ab376c0b774
Reqeique <95624174+Reqeique@users.noreply.github.com>
2025-11-08 15:48:09 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,19 +3,15 @@ set -e
 
 # Start Chrome bound to localhost so only the proxy can reach it
 chromium-browser \
-  --headless=new \
+  --headless \
   --remote-debugging-address=127.0.0.1 \
   --remote-debugging-port=9222 \
   --no-sandbox \
   --disable-dev-shm-usage \
-  # --disable-gpu \
-  # --disable-software-rasterizer \
+  --disable-gpu \
+  --disable-software-rasterizer \
   --remote-allow-origins=* \
-  --allow-insecure-localhost \
-  --disable-web-security \
-  --disable-features=IsolateOrigins,site-per-process &
+  --allow-insecure-localhost &
 
-  
-sleep 2
 # Run nginx in foreground
 exec nginx -g "daemon off;"
